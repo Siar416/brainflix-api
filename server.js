04aa9cff0182f1c195b1ece62cfc41298595baf9
Siar Wahidi <siar.wahidi@gmail.com>
2022-03-09 09:39:45 -0500

    added server file and assigned port

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+
+//show morgan biolerplate
+app.use(morgan("tiny"));
+
+//port
+const SERVER_PORT = 8000;
+
+// app listening on port 8000
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on port ${SERVER_PORT}`);
+});
