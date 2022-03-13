e4aae19bd0bbdcd0cd6ba2022b5630b5b66694f7
Siar Wahidi <siar.wahidi@gmail.com>
2022-03-13 18:18:18 -0400

    removed comments, removed morgan as i'm not using it

--- server.js
+++ server.js
@@ -2,26 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const morgan = require("morgan");
 const videosRoute = require("./routes/videos");
-// const { PORT } = process.env;
 
 app.use(express.static("./public"));
 
-//cors
 app.use(cors());
-
 app.use(express.json());
-
-//using routing by define endpoint
 app.use("/videos", videosRoute);
-
-//show morgan biolerplate
-app.use(morgan("tiny"));
 
 const SERVER_PORT = process.env.PORT || 8100;
 
-// app listening on port 8000
 app.listen(SERVER_PORT, () => {
   console.log(`Server is listening on port: ${SERVER_PORT}`);
 });
